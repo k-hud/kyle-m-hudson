138c3e2c8389d005aed9adc98c6e7bc73ce3dafb
Kyle Hudson <36278207+concept-theory@users.noreply.github.com>
2020-07-04 11:56:14 -0400

    Install Tailwind CSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,7 @@
+import "./src/components/tailwind.css";
+
 const { registerLinkResolver } = require('gatsby-source-prismic-graphql');
 const { linkResolver } = require('./src/utils/linkResolver');
  
 registerLinkResolver(linkResolver);
+
