fbf1f9bbc66461afdf37bb8d5288d6a53a7630bd
Kyle Hudson <36278207+concept-theory@users.noreply.github.com>
2020-07-06 09:14:59 -0400

    Tailwind working in develop not in production

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import "./src/components/tailwind.css";
+import "./src/stylesheets/tailwind.css";
 
 const { registerLinkResolver } = require('gatsby-source-prismic-graphql');
 const { linkResolver } = require('./src/utils/linkResolver');
